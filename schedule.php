067b1144f95d22bbee11be78a173b29ecb272898
Jan Carlo Cabantoc <111168221+Lospaduanos22@users.noreply.github.com>
2024-07-11 14:53:47 +0800

    updates

--- schedule.php
+++ schedule.php
@@ -31,13 +31,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Event Calendar</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.min.css">
-    <link href='https://fonts.googleapis.com/css?family=Exo 2' rel='stylesheet'>
-    <link rel="stylesheet" href="style.css">
+<?php
+    include('conn.php');
+    include('addData.php');
+    include('header.php');
+  ?>
 </head>
 <body>
     <!--Navbar-->
