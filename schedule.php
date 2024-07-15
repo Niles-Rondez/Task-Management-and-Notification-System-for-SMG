1c155ae8c24e6fcbf561be5027ea4acd6f163be9
CTRL-JUJU <andojude@gmail.com>
2024-07-15 14:49:55 +0800

    FUCK YEAH

--- schedule.php
+++ schedule.php
@@ -228,7 +228,6 @@ if ($result_tasks) {
             </div>
             <div class="modal-body">
                 <form method="POST" action="editData.php" id="updateTaskForm">
-                    <!-- Hidden input field for taskID -->
                     <input type="hidden" name="taskID" id="taskID">
 
                     <!-- Display taskID (not editable) -->
@@ -299,28 +298,7 @@ if ($result_tasks) {
                     
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-danger my-2" data-bs-toggle="modal" data-bs-target="#deleteTaskModal" id="deleteTaskButton"> Delete Task </button>
-                    <div class="modal fade" id="deleteTaskModal" tabindex="-1" aria-labelledby="deleteTaskModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="deleteTaskModalLabel">Confirm Task Deletion</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>Are you sure you want to delete this task?</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <!-- Form for deleting the task -->
-                                    <form action="deleteData.php" method="post">
-                                        <input type="hidden" name="taskID" id="deleteTaskID">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                                        <button type="submit" name="deleteTask" class="btn btn-danger">Delete Task</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        <button type="button" class="btn btn-danger my-2" data-bs-toggle="modal" data-bs-target="#deleteTaskModal" id="deleteTaskButton">Delete Task</button>
                         <button type="submit" class="btn btn-primary" name="submit">Save changes</button>
                     </div>
                 </form>
@@ -328,6 +306,35 @@ if ($result_tasks) {
         </div>
     </div>
 </div>
+
+<div class="modal fade" id="deleteTaskModal" tabindex="-1" aria-labelledby="deleteTaskModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteTaskModalLabel">Confirm Task Deletion</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to delete this task?</p>
+            </div>
+            <div class="modal-footer">
+                <form action="deleteData.php" method="post" id="deleteTaskForm">
+                    <input type="hidden" name="taskID" id="deleteTaskID">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" name="deleteTask" class="btn btn-danger">Delete Task</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    // JavaScript to set the task ID for deletion
+    $('#deleteTaskButton').on('click', function() {
+        var taskID = $('#taskID').val(); // Adjust this according to your actual task ID field
+        $('#deleteTaskID').val(taskID);
+    });
+</script>
         <div class="col-9">
         <div id='calendar-container' class="mx-0 pe-0">
             <div id='calendar' class=""></div>
