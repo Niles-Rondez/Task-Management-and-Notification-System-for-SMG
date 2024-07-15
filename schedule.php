440fe52c33cab6b3d4300d7142831961125c9ea2
CTRL-JUJU <andojude@gmail.com>
2024-07-15 10:27:29 +0800

    yes

--- schedule.php
+++ schedule.php
@@ -296,8 +296,8 @@ if ($result_tasks) {
                             <option value="High">High</option>
                         </select>
                     </div>
+                    <button type="button" class="btn btn-danger my-2" data-bs-toggle="modal" data-bs-target="#deleteTaskModal" id="deleteTaskButton"> Delete Task </button>
                     <div class="modal fade" id="deleteTaskModal" tabindex="-1" aria-labelledby="deleteTaskModalLabel" aria-hidden="true">
-
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
@@ -318,9 +318,6 @@ if ($result_tasks) {
                             </div>
                         </div>
                     </div>
-
-                    <button type="button" class="btn btn-danger my-2" data-bs-toggle="modal" data-bs-target="#deleteTaskModal" id="deleteTaskButton"> Delete Task </button>
-
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         <button type="submit" class="btn btn-primary" name="submit">Save changes</button>
